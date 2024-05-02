1e13d0d99b919b97d847e0b9620bb0d6102ef6f4
kubraaygun <kubraaygun@outlook.com.tr>
2024-05-02 21:13:42 +0300

    duzenleme

--- src/App.test.js
+++ src/App.test.js
@@ -1,43 +1,47 @@
 // App bileseninin testlerinin yazildigi test dosyasidir
 //bir dosyanin test dosyasi oldugunu anlamamiz icin dosya uzantısının
-// ...test.js / .test.jsx seklinde olmali 
+// ...test.js / .test.jsx seklinde olmali
 
 //unit Test
 //Yazılımın en küçük parçası görevini doğru şekilde yerine getiriyor mu
 // kontrolunu yapma.
 
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test ('ekrana react ogren yaz' , ()=>{
-  render (<App/>)
+test("ekrana react ogren yaz", () => {
+  render(<App />);
 
   // test edilecek elementleri cagir
-  const LinkEle=screen.getByText('Learn React')
+  const LinkEle = screen.getByText("React Öğren");
   //cagrilan elemandan beklentiyi belirt
-  expect(LinkEle).toBeInTheDocument()
-})
-
+  expect(LinkEle).toBeInTheDocument();
+});
 
 //
-test("Button tiklanma olayinda renk ve yazi degistirir", ()=>{
+test("Button tiklanma olayinda renk ve yazi degistirir", () => {
   //test edilecek bilesen ekrana basilir
-  render (<App/>)
+  render(<App />);
 
   //test edilecek butonu cagir
 
- const button= screen.getByRole("button")
-  //1-buton rengi kirmizidir 
-
+  const button = screen.getByRole("button");
+  //1-buton rengi kirmizidir
+  expect(button).toHaveStyle({ background: "red" });
 
   //2-butonun icinde "Maviye cevir" yazar
 
+  expect(button).toHaveTextContent("Maviye Çevir");
 
   //3-Butonun uzerine tiklanir
 
+  fireEvent.click(button);
+
   //4-Butonun rengi mavidir
+
+  expect(button).toHaveStyle({background:"aqua"})
 
   //5-Butonun icinde "kirmiziya cevir" yazar
 
-
-})
+  expect(button).toHaveTextContent('Kırmızıya Çevir')
+});
--- src/Button.jsx
+++ src/Button.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const Button = () => {
     const[isRed, setIsRed]=useState(true)
-  return <button onClick={()=>setIsRed(!isRed)} style={{ background: isRed ? 'red' : 'aqua' }}>{isRed ? "Maviye Cevir" : "Kirmiziya Cevir"}</button>;
+  return <button onClick={()=>setIsRed(!isRed)} style={{ background: isRed ? 'red' : 'aqua' }}>{isRed ? "Maviye Çevir" : "Kırmızıya Çevir"}</button>;
 };
 
 export default Button;
